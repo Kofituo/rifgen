5d62b57eb85b87fd4d916f011a4dfb72aa564a85
Kofituo <58628654+Kofituo@users.noreply.github.com>
2021-10-06 22:32:22 +0000

    Delete gen_attributes/target/debug directory

--- gen_attributes/target/debug/build/proc-macro2-15c6b64d2ed182be/build_script_build-15c6b64d2ed182be.d
+++
@@ -1,5 +0,0 @@
-/home/kofi/IdeaProjects/rust_interface_file_generator/gen_attributes/target/debug/build/proc-macro2-15c6b64d2ed182be/build_script_build-15c6b64d2ed182be: /home/kofi/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.19/build.rs
-
-/home/kofi/IdeaProjects/rust_interface_file_generator/gen_attributes/target/debug/build/proc-macro2-15c6b64d2ed182be/build_script_build-15c6b64d2ed182be.d: /home/kofi/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.19/build.rs
-
-/home/kofi/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.19/build.rs:
--- gen_attributes/target/debug/deps/gen_attributes-e191e11d7626998b.d
+++
@@ -1,5 +0,0 @@
-/home/kofi/IdeaProjects/rust_interface_file_generator/gen_attributes/target/debug/deps/gen_attributes-e191e11d7626998b.rmeta: src/lib.rs
-
-/home/kofi/IdeaProjects/rust_interface_file_generator/gen_attributes/target/debug/deps/gen_attributes-e191e11d7626998b.d: src/lib.rs
-
-src/lib.rs:
